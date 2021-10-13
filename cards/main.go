e2b07fe0e7ff9598f471062fcb75e05647b457ef
Luca Lanziani <luca@lanziani.com>
2021-10-13 15:17:41 +0200

    newDeck function

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
+	// cards = append(cards, "Six of Spades")
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamons"
 }
