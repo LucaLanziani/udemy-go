d4e67f0a2ee3f6f1eb59ca0e238ec53cfb7a3534
Luca Lanziani <luca@lanziani.com>
2021-10-14 22:13:54 +0200

    Http example, Reader/Writer

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,7 +16,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	body := make([]byte, 99999)
-	resp.Body.Read(body)
-	fmt.Println(string(body))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Printf("Received %v bytes", len(bs))
+	return len(bs), nil
 }
