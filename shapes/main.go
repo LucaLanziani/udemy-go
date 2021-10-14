c4fcb72f7948cd3dfaa4c1b05e56e69efe36763a
Luca Lanziani <luca@lanziani.com>
2021-10-14 22:14:17 +0200

    Implement shapes assignment

--- shapes/main.go
+++ shapes/main.go
@@ -2,17 +2,32 @@ package main
 
 import "fmt"
 
+type square struct {
+	side float32
+}
+
+type triangle struct {
+	base   float32
+	height float32
+}
+
+func (t triangle) getArea() float64 {
+	return float64(t.base * t.height / 2)
+}
+
+func (s square) getArea() float64 {
+	return float64(s.side * s.side)
+}
+
 type shape interface {
-	area() float32
+	getArea() float64
 }
 
 func printArea(s shape) {
-	fmt.Println(s.area())
+	fmt.Println(s.getArea())
 }
 
 func main() {
 	printArea(square{4})
-	printArea(circle{2})
 	printArea(triangle{2, 2})
-
 }
--- shapes/square.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type square struct {
-	side float32
-}
-
-func (s square) area() float32 {
-	return s.side * s.side
-}
