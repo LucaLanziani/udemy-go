f022ecd2071890144cecb45e7730d1607be4b523
Luca Lanziani <luca@lanziani.com>
2021-10-14 15:11:29 +0200

    printMap

--- map/main.go
+++ map/main.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main() {
 	// var colors map[string]string
-	colors := make(map[string]string)
-	// colors := map[string]string{
-	// 	"red":   "#FF0000",
-	// 	"green": "#00FF00",
-	// 	"blue":  "#0000FF",
-	// }
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+		"blue":  "#0000FF",
+		"white": "#000000",
+		"black": "#000000",
+	}
 	colors["white"] = "#FFFFFF"
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println(key, value)
+	}
 }
