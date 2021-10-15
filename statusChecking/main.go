f70275d0623d0b0ab04ed1daae0663a0da381d78
Luca Lanziani <luca@lanziani.com>
2021-10-15 11:25:58 +0200

    Status checking with channels

--- statusChecking/main.go
+++ statusChecking/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,16 +13,27 @@ func main() {
 		"http://lanziani.com",
 	}
 
+	c := make(chan string)
+
 	for _, site := range sites {
-		checkWebSite(site)
+		go checkWebSite(site, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(time.Second)
+			checkWebSite(link, c)
+		}(l)
 	}
 }
 
-func checkWebSite(site string) {
+func checkWebSite(site string, c chan string) {
 	_, err := http.Get(site)
 	if err != nil {
 		fmt.Println(site, "seems down")
+		c <- site
 		return
 	}
 	fmt.Println(site, "it's responding")
+	c <- site
 }
